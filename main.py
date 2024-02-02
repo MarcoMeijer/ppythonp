ff632a2fe05fea994a4aa9e85ac76d3b93dd95c5
Marco Meijer <marcomeijerm@gmail.com>
2024-02-02 15:55:31 +0100

    feat: parsing integers

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import argparse
+from typing import Union
 
-def tokenize(input: str) -> list[str]:
-    return [x.strip() for x in input.split(" ")]
+class Token:
+    def __init__(self, value: str, line: int):
+        self.value = value
+        self.line = line
+
+class Literal:
+    def __init__(self, value: int) -> None:
+        self.value = value
+
+class CompileError:
+    def __init__(self, line, message) -> None:
+        self.line = line
+        self.message = message
+
+    def display(self, lines: list[str]) -> None:
+        print(f"Compile error on line {self.line}:")
+        print(" --> " + lines[self.line])
+        print(self.message)
+
+def tokenize(input: str) -> list[Token]:
+    lines = input.split("\n")
+    result = []
+    for line in range(len(lines)):
+        result.extend([Token(x, line) for x in lines[line].split(" ")])
+    return result
+
+def parse_int(input: list[Token]) -> Union[tuple[Literal, list[Token]], CompileError]:
+    if input[0].value.isdigit():
+        return (Literal(int(input[0].value)), input[1:])
+    return CompileError(input[0].line, "Expected integer")
 
 parser = argparse.ArgumentParser("pythonpp")
 parser.add_argument("filename")
@@ -9,4 +38,9 @@ args = parser.parse_args()
 
 file = open(args.filename, 'r')
 source = file.read()
-print(tokenize(source))
+result = parse_int(tokenize(source))
+
+if isinstance(result, CompileError):
+    result.display(source.split("\n"))
+if isinstance(result, tuple):
+    print(result[0].value)
