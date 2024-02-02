bcdb4f813535c2a2703558459008989a93ab69dd
Marco Meijer <marcomeijerm@gmail.com>
2024-02-02 16:14:38 +0100

    feat: parsing addition

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import argparse
-from typing import Union
 
 class Token:
     def __init__(self, value: str, line: int):
         self.value = value
         self.line = line
 
-class Literal:
+class Node:
+    pass
+
+class Literal(Node):
     def __init__(self, value: int) -> None:
         self.value = value
+
+    def __str__(self) -> str:
+       return str(self.value)
+
+class Add(Node):
+    def __init__(self, lhs: Node, rhs: Node) -> None:
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def __str__(self) -> str:
+        return str(self.lhs) + "+" + str(self.rhs)
 
 class CompileError:
     def __init__(self, line, message) -> None:
@@ -27,10 +40,38 @@ def tokenize(input: str) -> list[Token]:
         result.extend([Token(x, line) for x in lines[line].split(" ")])
     return result
 
-def parse_int(input: list[Token]) -> Union[tuple[Literal, list[Token]], CompileError]:
+def parse_token(token):
+    def parser(input: list[Token]) -> tuple[str, list[Token]] | CompileError:
+        if input[0].value == token:
+            return input[0].value, input[1:]
+        return CompileError(input[0].line, "Expected +")
+    return parser
+
+def parse_int(input: list[Token]) -> tuple[Node, list[Token]] | CompileError:
     if input[0].value.isdigit():
-        return (Literal(int(input[0].value)), input[1:])
+        return Literal(int(input[0].value)), input[1:]
     return CompileError(input[0].line, "Expected integer")
+
+def parse_plus(input: list[Token]) -> tuple[Node, list[Token]] | CompileError:
+    result = parse_int(input)
+    if isinstance(result, CompileError):
+        return result
+    lhs, input = result
+
+    if input == []:
+        return lhs, input
+
+    result = parse_token("+")(input)
+    if isinstance(result, CompileError):
+        return lhs, input
+    input = result[1]
+
+    result = parse_plus(input)
+    if isinstance(result, CompileError):
+        return result
+    rhs, input = result
+
+    return Add(lhs, rhs), input
 
 parser = argparse.ArgumentParser("pythonpp")
 parser.add_argument("filename")
@@ -38,9 +79,9 @@ args = parser.parse_args()
 
 file = open(args.filename, 'r')
 source = file.read()
-result = parse_int(tokenize(source))
+result = parse_plus(tokenize(source))
 
 if isinstance(result, CompileError):
     result.display(source.split("\n"))
 if isinstance(result, tuple):
-    print(result[0].value)
+    print(result[0])
