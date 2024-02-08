6fd38fcf596cf75277ca3d72bc64807d7ce176e4
Marco Meijer <marcomeijerm@gmail.com>
2024-02-08 13:25:54 +0100

    tests: refactor using run_test

--- src/tests.py
+++ src/tests.py
@@ -1,12 +1,18 @@
 import unittest
 import subprocess
 
+def run_test(test_case: unittest.TestCase, file_name: str):
+    source_file = f"{file_name}.ppp"
+    output_file = f"{file_name}.out"
+    result = subprocess.run(["python3.12", "src/main.py", source_file], stdout=subprocess.PIPE)
+    file = open(output_file, 'r')
+    expected_output = file.read()
+    file.close()
+    test_case.assertEqual(str(result.stdout, encoding='utf-8'), expected_output)
+
 class TestStringMethods(unittest.TestCase):
     def test_hello_world(self):
-        result = subprocess.run(["python3.12", "src/main.py", "tests/hello_world.ppp"], stdout=subprocess.PIPE)
-        file = open("tests/hello_world.out", 'r')
-        expected_output = file.read()
-        self.assertEqual(str(result.stdout, encoding='utf-8'), expected_output)
+        run_test(self, "tests/hello_world")
 
 if __name__ == '__main__':
     unittest.main()
